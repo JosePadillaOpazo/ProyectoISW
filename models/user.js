358ec120fdc1a69a86f66ec3ba0cf4a2d3c6285c
= <sebastian_morenoi@hotmail.com>
2022-11-27 21:28:41 -0300

    Login fix v3

--- models/user.js
+++ models/user.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const UserSchema = new Schema({
     rut:{
-        type:Schema.ObjectId,
-        references: { type : [Schema.Types.ObjectId], refPath: 'model_type'},
-        model_type: {type: String, enum: ['apoderado', 'Educadora', 'asistente']}
+        type: [Schema.Types.ObjectId],
+        refPath: 'model_type'
+    },
+    model_type:{
+        type: String,
+        enum: ['Educadora', 'Apoderado', 'asistente']
     }
 })
 
