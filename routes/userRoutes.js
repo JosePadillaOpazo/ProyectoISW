ad1478e40032bae15d1b9a00a8ed872e243124f0
= <sebastian_morenoi@hotmail.com>
2022-11-27 23:38:48 -0300

    User beta

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController')
 api.post('/user', userController.addUser)
 api.get('/users', userController.getUser)
 api.get('/usersP', userController.getUserPow)
+api.delete('/user/delete/:id', userController.delUser)
 
 module.exports = api
