ba0f539988bd3fe8c3f8c6c4bcae7a3fc4fa3436
Vyndrean <sebastian_morenoi@hotmail.com>
2023-01-02 05:14:53 -0300

    Bug fix v0.2.3

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -16,23 +16,7 @@ const Navbar = () => {
                     <Link as={NextLink} href='/Educadora/VistaEducadoras'>Educadora</Link>
                 </li>
                 <li>
-                    <Link as={NextLink} href='/asistente'>Asistente</Link>
-                </li>
-                <li>
-                    <Link as={NextLink} href='/apoderado'>Apoderado</Link>
-                </li>
-                <li>
-                    <Link as={NextLink} href='/asistencia'>Asistencia</Link>
-                </li>
-                <li>
                     <Link as={NextLink} href='/Rutina/VistaRutinas'>Rutina</Link>
-                </li>
-
-                <li>
-                    <Link as={NextLink} href='/enfermedad'>Enfermedad</Link>
-                </li>
-                <li>
-                    <Link as={NextLink} href='/antecedente'>Antecedente</Link>
                 </li>
             </ul>
         </nav>
